ebd407d41bb8eae9efd28c404b69b586fa66a162
Erik Ziegler <erik.sweed@gmail.com>
2018-07-13 10:26:45 +0200

    Move WADO to both client and server

---
+++ Packages/ohif-dicom-services/both/DICOMWeb/makeRequest.js
@@ -0,0 +1,42 @@
+import { Meteor } from "meteor/meteor";
+import { OHIF } from 'meteor/ohif:core';
+
+async function makeRequest(geturl, options) {
+    const parsed = new URL(geturl);
+
+    let requestOpt = {
+        method: 'GET'
+    };
+
+    // TODO: Clean this up
+    const accessToken = Meteor.user().services.keycloak.accessToken;
+    if (accessToken) {
+        requestOpt.headers = {
+            Accept: 'application/json',
+            Authorization: `Bearer ${accessToken}`
+        };
+    }
+
+    if (options.headers) {
+        Object.keys(options.headers).forEach(key => {
+            requestOpt.headers[key] = options.headers[key];
+        });
+    }
+
+    return new Promise((resolve, reject) => {
+        fetch(parsed.href, requestOpt).then((response) => {
+            if (response.status >= 400) {
+                reject(new Error(response.status));
+            }
+
+            resolve(response.json());
+
+        }, (error) => {
+            OHIF.log.error('There was an error in the DICOMWeb Server');
+
+            reject(new Error(error));
+        });
+    });
+}
+
+export default makeRequest;
--- Packages/ohif-studies/imports/server/services/namespace.js
+++ Packages/ohif-studies/imports/server/services/namespace.js
@@ -1,8 +1,6 @@
 import { OHIF } from 'meteor/ohif:core';
 
 const Services = {};
-Services.QIDO = {};
-Services.WADO = {};
 Services.DIMSE = {};
 
 OHIF.studies.services = Services;
